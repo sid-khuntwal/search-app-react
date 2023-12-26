8680e0fc1e19b84f85cee01f4a25a3b976b7f8d8
Siddharth Khuntwal <achillesid34@gmail.com>
2023-12-26 15:49:36 +0530

    Faker Data

---
+++ src/Assets/FakerData.js
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker";
+const brand = ["Levis", "Max"];
+const fakerData = [];
+for (let i = 0; i < 12; i++) {
+    fakerData.push({
+        img: faker.image.urlPicsumPhotos({
+            category: "dark",
+            width: 239,
+            height: 325,
+        }),
+        brand: brand[Math.floor(Math.random() * 2)],
+        star: 5,
+        prodName: faker.commerce.productName(),
+        price: faker.commerce.price({
+            min: 300,
+            max: 1200,
+            dec: 0,
+            symbol: "Rs.",
+        }),
+        customerNumber: faker.random.numeric(3),
+    });
+}
+
+export default fakerData;
