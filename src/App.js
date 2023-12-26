08e21ee3b27f61b113f98c750baf53a3fc072403
Siddharth Khuntwal <achillesid34@gmail.com>
2023-12-26 15:41:35 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import SearchBox from './Components/SearchBox';
+import logo from "./Assets/logo.jpg";
+
 function App() {
   return (
     <div className="App">
-
-      <SearchBox />
+      <div id="zevi-icon">
+        <img src={logo} alt="logo" width="86px" height="45px" />
+      </div>
+      <div className="search-box-container">
+        <SearchBox
+        />
+      </div>
     </div>
+
   );
 }
 
